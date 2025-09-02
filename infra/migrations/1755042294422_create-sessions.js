54500e77ab75270c0ac58e6e0a4c3afa37b3fd95
gustavolimav <gustavolvma@gmail.com>
2025-09-01 21:52:00 -0300

    fix: ajust the code for migrations

--- infra/migrations/1755042294422_create-sessions.js
+++ infra/migrations/1755042294422_create-sessions.js
@@ -1,14 +1,6 @@
-/**
- * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
- */
 export const shorthands = undefined;
 
-/**
- * @param pgm {import('node-pg-migrate').MigrationBuilder}
- * @param run {() => void | undefined}
- * @returns {Promise<void> | void}
- */
-export const up = (pgm) => {
+export function up(pgm) {
   pgm.createTable("sessions", {
     id: {
       type: "uuid",
@@ -41,4 +33,4 @@ export const up = (pgm) => {
       default: pgm.func("now()"),
     },
   });
-};
+}
